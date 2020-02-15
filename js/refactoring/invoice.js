d0fecec2fbe7f6bab6c180672bdbceaef2d11c0b
mochiko <mochiko@pepabo.com>
2020-02-15 16:37:19 +0900

    関数のインライン化

--- js/refactoring/invoice.js
+++ js/refactoring/invoice.js
@@ -49,14 +49,13 @@ function statement (invoice, plays) {
                             }).format;
 
     for (let perf of invoice.perfomances) {
-        let thisAmount = amountFor(perf);
         //ボリューム特典のポイントを加算する
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 喜劇の時は10人につきさらにポイントを追加
         if ("comedy" == playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
         // 注文の内訳を出力
-        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${format(amountFor(pref)/100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`
     result += `You earned ${volumeCredits} credeits\n`;
