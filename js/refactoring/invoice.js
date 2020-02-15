3c61908bfd3515438ad35511c1a2c81574d47f98
mochiko <mochiko@pepabo.com>
2020-02-15 16:26:05 +0900

    変数のインライン化

--- js/refactoring/invoice.js
+++ js/refactoring/invoice.js
@@ -48,17 +48,15 @@ function statement (invoice, plays) {
                                 minimumFractionDigits: 2
                             }).format;
 
-    for (let perf of invoice.perfomances[0]) {
-        // 問い合わせによる一次変数の置き換えを行った
-        const play = playFor(perf);
+    for (let perf of invoice.perfomances) {
         //切り出した関数を呼び出す
-        let thisAmount = amountFor(perf, play);
+        let thisAmount = amountFor(perf, playFor(perf));
         //ボリューム特典のポイントを加算する
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 喜劇の時は10人につきさらにポイントを追加
-        if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" == playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
         // 注文の内訳を出力
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`
