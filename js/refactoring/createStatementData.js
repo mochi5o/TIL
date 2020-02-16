8c87b93a47f680ec49004d7601f08d37cd7fd639
mochi5o <1610.baumkuchen.124@gmail.com>
2020-02-17 00:28:26 +0900

    演目別の金額計算箇所を切り出し

--- js/refactoring/createStatementData.js
+++ js/refactoring/createStatementData.js
@@ -58,28 +58,6 @@ export default function createStatementData(invoice, plays){
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance){
-        let result = 0;
-        switch (aPerformance.play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result =  30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
-
     function volumeCreditsFor(aPerformance){
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
@@ -100,5 +78,26 @@ class PerformanceCalculator {
     constructor(aPerformance) {
         this.performance = aPerformance;
         this.play = aPlay;
+    }
+    get amount() {
+        let result = 0;
+        switch (this.play.type) {
+            case "tragedy":
+            result = 40000;
+            if (this.performances.audience > 30) {
+                result += 1000 * (this.performance.audience - 30);
+            }
+            break;
+        case "comedy":
+            result =  30000;
+            if (this.performance.audience > 20) {
+                result += 10000 + 500 * (this.performance.audience - 20);
+            }
+            result += 300 * this.performance.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result;
     }
 }
