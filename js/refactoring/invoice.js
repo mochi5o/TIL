e173a6b8aec300a57959a106df76608ff0ae5572
mochi5o <1610.baumkuchen.124@gmail.com>
2020-02-16 20:24:10 +0900

    合計値の算出関数を移動

--- js/refactoring/invoice.js
+++ js/refactoring/invoice.js
@@ -41,7 +41,26 @@ function statement (invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
+
+    function totalAmount(data){
+        let result = 0;
+        for (let perf of data.performances){
+            result += perf.amount;
+        }
+        return result;
+    }
+
+
+    function totalVolumeCredits(data){
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+        return result;
+    }
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
@@ -90,26 +109,13 @@ function renderPlainText(data, invoice, plays){
     for (let perf of data.performances) {
         result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
     }
-    result += `Amount owed is ${usd(totalAmount())}\n`
-    result += `You earned ${totalVolumeCredits()} credeits\n`;
+    result += `Amount owed is ${usd(data.totalAmount)}\n`
+    result += `You earned ${data.totalVolumeCredits} credeits\n`;
     console.log(result);
     return result;
 
-    function totalAmount(){
-        let result = 0;
-        for (let perf of data.performances){
-            result += perf.amount;
-        }
-        return result;
-    }
 
-    function totalVolumeCredits(){
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
-    }
+
 
     function usd(aNumber){
         return new Intl.NumberFormat('en-US',
