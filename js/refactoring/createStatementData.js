864a56aecf0a1b86baf5a58626f6d50e20eead93
mochi5o <1610.baumkuchen.124@gmail.com>
2020-02-16 21:23:01 +0900

    クラスを作成

--- js/refactoring/createStatementData.js
+++ js/refactoring/createStatementData.js
@@ -46,8 +46,9 @@ export default function createStatementData(invoice, plays){
     return result;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
+        result.play = calculator.play;
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
         return result;
@@ -92,5 +93,12 @@ export default function createStatementData(invoice, plays){
 
     function totalVolumeCredits(data) {
         return data.performances.reduce((total, p) => p.volumeCredits, 0);
+    }
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance) {
+        this.performance = aPerformance;
+        this.play = aPlay;
     }
 }
