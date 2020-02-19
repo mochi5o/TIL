3555c9e47976a858f772f11bf4167b9bb4be9c64
mochi5o <1610.baumkuchen.124@gmail.com>
2020-02-19 23:50:18 +0900

    リファクタリング完成ー第1章終わり

--- js/refactoring/createStatementData.js
+++ js/refactoring/createStatementData.js
@@ -1,4 +1,3 @@
-alert('ok');
 const invoice = [
     {
         "customer": "BigCo",
@@ -35,14 +34,13 @@ const plays = {
         "type": "tragedy"
     }
 };
-console.log(plays.hamlet.name);
 
 export default function createStatementData(invoice, plays){
     const result = {};
     result.customer = invoice.customer;
     result.performances = invoice.performances.map(enrichPerformance);
-    result.totalAmount = totalAmount(statementData);
-    result.totalVolumeCredits = totalVolumeCredits(statementData);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
     return result;
 
     function enrichPerformance(aPerformance) {
@@ -58,66 +56,55 @@ export default function createStatementData(invoice, plays){
         return plays[aPerformance.playID];
     }
 
-    function volumeCreditsFor(aPerformance){
-        return new PerformanceCalculator(aPerformance, volumeCreditsFor(aPerformance)).volumeCredits;
-    }
-
-    function amountFor(aPerformance){
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }
-
     function totalAmount(data){
         return data.performances.reduce((total, p) => total + p.amount, 0);
     }
 
     function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => p.volumeCredits, 0);
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch(aPlay, type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+        throw new Error(`未知の演劇の種類: ${aPlay.type}`);
+    }
+}
 class PerformanceCalculator {
-    constructor(aPerformance) {
+    constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
         this.play = aPlay;
     }
     get amount() {
         throw new Error('subclass responsibility');
     }
-
     get volumeCredits(){
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
+        return Math.max(this.performance.audience -30, 0);
     }
 }
 
-function createPerformanceCalculator(aPerformance, aPlay) {
-    switch(aPlay, type) {
-    case "tragedy": return new TragedyCalculatora(aPerformance, aPlay);
-    case "comedy": return new ComedyCalculatora(aPerformance, aPlay);
-    default:
-        throw new Error(`未知の演劇の種類: ${aPlay.type}`);
-    }
-}
-
-class TragedyCalculatora extends PerformanceCalculator{
+class TragedyCalculator extends PerformanceCalculator{
     get amount() {
-        result = 40000;
+        let result = 40000;
         if (this.performances.audience > 30) {
             result += 1000 * (this.performance.audience - 30);
         }
         return result;
     }
 }
-class ComedyCalculatora extends PerformanceCalculator{
+class ComedyCalculator extends PerformanceCalculator{
     get amount() {
-        result =  30000;
+        let result =  30000;
         if (this.performance.audience > 20) {
             result += 10000 + 500 * (this.performance.audience - 20);
         }
         result += 300 * this.performance.audience;
         return result;
     }
-
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
 }
