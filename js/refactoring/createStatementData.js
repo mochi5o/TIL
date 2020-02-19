dc2516a7ca609b03a4f6025662c888365cddfa88
mochi5o <1610.baumkuchen.124@gmail.com>
2020-02-19 19:37:47 +0900

    メソッドをサブクラスへ

--- js/refactoring/createStatementData.js
+++ js/refactoring/createStatementData.js
@@ -81,25 +81,7 @@ class PerformanceCalculator {
         this.play = aPlay;
     }
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-            result = 40000;
-            if (this.performances.audience > 30) {
-                result += 1000 * (this.performance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result =  30000;
-            if (this.performance.audience > 20) {
-                result += 10000 + 500 * (this.performance.audience - 20);
-            }
-            result += 300 * this.performance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result;
+        throw new Error('subclass responsibility');
     }
 
     get volumeCredits(){
@@ -111,6 +93,31 @@ class PerformanceCalculator {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch(aPlay, type) {
+    case "tragedy": return new TragedyCalculatora(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculatora(aPerformance, aPlay);
+    default:
+        throw new Error(`未知の演劇の種類: ${aPlay.type}`);
+    }
 }
 
+class TragedyCalculatora extends PerformanceCalculator{
+    get amount() {
+        result = 40000;
+        if (this.performances.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+class ComedyCalculatora extends PerformanceCalculator{
+    get amount() {
+        result =  30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+
+}
