69b82ed748a017cee58d655f3a08fa7e370e273c
mochi5o <1610.baumkuchen.124@gmail.com>
2020-03-15 19:19:02 +0900

    diary.shの完成

--- ScrapBox/Linux/diary.sh
+++ ScrapBox/Linux/diary.sh
@@ -3,4 +3,18 @@
 # 日記を書くためのスクリプト
 # 日付はYYYY-MM-DD方式で表記して2020-01-01.txtのようなファイル名で保存
 
-vim $(date '+%Y-%m-%d').txt
+# 日記データの保存ディレクトリ
+directory="${HOME}/diary"
+
+# データ保存ディレクトリがなければ作成する
+if [ ! -d "$directory" ]; then
+    mkdir "$directory"
+fi
+
+diaryfile= "${directory}/$(date '+%Y-%m-%d').txt"
+
+if [ ! -e "$diaryfile" ]; then
+    date '+%Y/%m/%d' > "$diaryfile"
+fi
+
+vi "$diaryfile"
