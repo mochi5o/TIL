e31ac4a3f0ded5d6bb316b95650a6a3a31bb10d3
mochi5o <1610.baumkuchen.124@gmail.com>
2020-03-21 22:33:56 +0900

    gameクラスにstateを置いて、boardクラスのstateを削除

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -11,18 +11,11 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        }
-    }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
-            return
+            return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
@@ -34,8 +27,8 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
-            value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
@@ -73,11 +66,20 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
     render() {
         return (
         <div className="game">
             <div className="game-board">
-            <Board />
+                <Board />
             </div>
             <div className="game-info">
                 <div>{/* status */}</div>
