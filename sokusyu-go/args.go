664ae752941ee0b72e6641360c1e0bbce0409197
mochi5o <1610.baumkuchen.124@gmail.com>
2021-01-30 00:46:44 +0900

    constとiota

--- sokusyu-go/args.go
+++ sokusyu-go/args.go
@@ -12,10 +12,16 @@ func main() {
   fmt.Println(a, b, c, d)
   // 0 1 2 3
 
-  // 定義済みの変数に再代入できない
+  // :=では定義済みの変数に再代入できない
+  // :=は宣言と代入を一緒に実行する（var省略)
   a := 5
   fmt.Println(a)
   // ./main.go:16:5: no new variables on left side of :=
+
+  // 複数同時に定義
+  y, z := 10, 14
+  fmt.Println(y, z)
+  // 10 14
 }
 
 // main関数外で:=を用いた変数宣言ができない
