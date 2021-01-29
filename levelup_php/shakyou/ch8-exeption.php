c3c6b5938d61d84af93dfb89f8fa42aafe513586
mochi5o <1610.baumkuchen.124@gmail.com>
2021-01-29 23:36:22 +0900

    init

--- levelup_php/shakyou/ch8-exeption.php
+++ levelup_php/shakyou/ch8-exeption.php
@@ -3,7 +3,7 @@
 final class SampleException extends \Exception{}
 
 try {
-    if (1 !=== 0){
+    if (1 !== 0){
         throw new SampleException('1 is not 0');
     }
 } catch(\SampleException $e) {
---
+++ sokusyu-go/README.md
@@ -0,0 +1,9 @@
+# Goやってみる
+
+環境メモ
+
+```
+~ ❯❯❯ go version
+go version go1.14.2 darwin/amd64
+~ ❯❯❯
+```
