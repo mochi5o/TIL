4f8f7d69db8a83b9d9d6ad15351c4e34bc62a693
mochi5o <1610.baumkuchen.124@gmail.com>
2021-02-11 14:12:22 +0900

    -lで整列できるようになった

--- ruby-practices/ruby_ls.rb
+++ ruby-practices/ruby_ls.rb
@@ -7,7 +7,7 @@ base_dir = Dir.getwd
 arr = []
 file_list = Dir.entries(base_dir)
 file_list.each do |file|
-  # file = "#{file}/" if Dir.exist?("#{base_dir}/#{file}")
+  file = "#{file}/" if Dir.exist?("#{base_dir}/#{file}")
   arr.push file
 end
 
@@ -27,24 +27,25 @@ def with_r_opt
 end
 
 # いいかんじで並べるための処理
-max_length = arr.sort_by(&:length).last.length
-arr.map! do |s|
-  s.ljust(max_length + 2)
+def with_l_output
+  max_length = arr.sort_by(&:length).last.length
+  arr.map! do |s|
+    s.ljust(max_length + 2)
+  end
+
+  remainder = arr.length % 3
+  if arr.length % 3 == 1
+    arr.push('','')
+  elsif arr.length % 3 == 2
+    arr.push('')
+  end
+
+  without_l_opt_show = arr.each_slice(arr.length.div(3)).to_a.transpose
+
+  without_l_opt_show.each do |a|
+  puts a.join('')
+  end
 end
-
-remainder = arr.length % 3
-if arr.length % 3 == 1
-  arr.push('','')
-elsif arr.length % 3 == 2
-  arr.push('')
-end
-
-without_l_opt_show = arr.each_slice(arr.length.div(3)).to_a.transpose
-
-without_l_opt_show.each do |a|
-puts a.join('')
-end
-
 
 def with_l_output
 
@@ -62,33 +63,33 @@ types = {
       'socket' => 's'
 }
 
-# total = 0
-# arr.each do |f|
-#   total += File.stat(f).blocks
-# end
+total = 0
+arr.each do |f|
+  total += File.stat(f).blocks
+end
 
 
-# str = ''
-# arr.each do |f|
-#   stat = File.stat(f)
-#   # blocks += stat.blocks
-#   mode_arr = stat.mode.to_s(8).split(//)[-3..]
-#   mode = ''
-#   mode_arr.each do |n|
-#     mode += permit[n.to_i]
-#   end
-#   type = types[stat.ftype]
-#   nlink = stat.nlink
-#   owner = Etc.getpwuid(stat.uid).name
-#   gid   = stat.gid
-#   size  = stat.size
-#   mtime = stat.mtime
-#   month = month_name[mtime.month- 1]
-#   day   = mtime.day
-#   hour  = mtime.hour
-#   min   = mtime.min
-#   str += "#{type}#{mode} #{nlink} #{owner}  #{gid}  #{size} #{month}  #{day} #{hour}:#{min} #{f}\n"
-# end
+str = ''
+arr.each do |f|
+  stat = File.stat(f)
+  # f = "#{f}/" if stat
+  mode_arr = stat.mode.to_s(8).split(//)[-3..]
+  mode = ''
+  mode_arr.each do |n|
+    mode += permit[n.to_i]
+  end
+  type = types[stat.ftype]
+  nlink = stat.nlink.to_s.rjust(3)
+  owner = Etc.getpwuid(stat.uid).name
+  gid   = stat.gid
+  size  = stat.size.to_s.rjust(6)
+  mtime = stat.mtime
+  month = month_name[mtime.month- 1]
+  day   = mtime.day.to_s.rjust(2)
+  hour  = mtime.hour.to_s.rjust(2, "0")
+  min   = mtime.min.to_s.rjust(2, "0")
+  str += "#{type}#{mode} #{nlink} #{owner}  #{gid}  #{size} #{month}  #{day} #{hour}:#{min} #{f}\n"
+end
 
-# puts "total #{total}"
-# puts str
+puts "total #{total}"
+puts str
