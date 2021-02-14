92b6e5e31022b8ffa8c67bc9eb864553169f05c6
mochi5o <1610.baumkuchen.124@gmail.com>
2021-02-14 20:07:33 +0900

    大きさを変える処理

--- canvastest/test-kanva/src/index.js
+++ canvastest/test-kanva/src/index.js
@@ -1,7 +1,94 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Stage, Layer, Rect, Star, Circle, Text } from 'react-konva';
+import { Stage, Layer, Rect, Star, Circle, Text, Transformer } from 'react-konva';
 import Konva from 'konva';
+
+const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  return (
+    <React.Fragment>
+      <Rect
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+const initialRectangles = [
+  {
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 100,
+    fill: 'red',
+    id: 'rect1',
+    stroke: 'black',
+    strokeWidth: 4,
+  },
+  {
+    x: 150,
+    y: 150,
+    width: 100,
+    height: 100,
+    fill: 'green',
+    id: 'rect2',
+  },
+];
+
 
 class RenderRect extends React.Component {
   state = {
@@ -15,10 +102,10 @@ class RenderRect extends React.Component {
   render() {
     return (
       <Rect
-        x={100}
-        y={100}
-        width={500}
-        height={500}
+        x={400}
+        y={400}
+        width={50}
+        height={50}
         fill={this.state.color}
         shadowBlur={5}
         onClick={this.handleClick}
@@ -48,23 +135,75 @@ class RenderStar extends React.Component {
   }
 }
 
-class App extends Component {
+class RenderCircle extends React.Component {
   render() {
+    return (
+      <Circle
+        x={300}
+        y={50}
+        radius={50}
+        fill="blue"
+        opacity={0.8}
+        draggable
+        shadowColor="black"
+        shadowBlur={10}
+        shadowOpacity={0.6}
+      />
+    );
+  }
+
+}
+const App = () => {
+  const [rectangles, setRectangles] = React.useState(initialRectangles);
+  const [selectedId, selectShape] = React.useState(null);
+
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
     // Stage is a div container
     // Layer is actual canvas element (so you may have several canvases in the stage)
     // And then we have canvas shapes inside the Layer
     return (
       <div style={{"width": "60%"}}>
-        <Stage width={500} height={500} style={{"border-style": "solid"}}>
+        <Stage
+          width={500}
+          height={500}
+          style={{"border-style": "solid"}}
+          onMouseDown={checkDeselect}
+          onTouchStart={checkDeselect}
+        >
           <Layer>
+            {rectangles.map((rect, i) => {
+              return (
+                <Rectangle
+                  key={i}
+                  shapeProps={rect}
+                  isSelected={rect.id === selectedId}
+                  onSelect={() => {
+                    selectShape(rect.id);
+                  }}
+                  onChange={(newAttrs) => {
+                    const rects = rectangles.slice();
+                    rects[i] = newAttrs;
+                    setRectangles(rects);
+                  }}
+                />
+              );
+            })}
             <Text text="Try click on rect" />
             <RenderRect />
             <RenderStar />
+            <RenderCircle />
           </Layer>
         </Stage>
+        <button>add Rect</button>
+        <button>add Circle</button>
       </div>
     );
-  }
 }
 
 render(<App />, document.getElementById('root'));
